314851ea6dcd4ddba398f81a4582c00ff0dd550d
rodrigocarreonc <rodrigocarreon21@gmail.com>
2026-05-20 20:12:05 -0600

    Creacion de OperationsController con funcion squareRoot

---
+++ app/Http/Controllers/OperationsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OperationsController extends Controller
+{
+    // addition
+
+    // subtraction
+
+    // multiplication
+
+    // division
+
+    // square
+
+    // square root
+    public function squareRoot($a): float
+    {
+        return sqrt($a);
+    }
+
+    // factorial
+}
